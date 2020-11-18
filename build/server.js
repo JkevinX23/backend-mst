9dbb02b0ad7a9175178237a03e23d30bd990790a
jefesonk1 <jefesonk1@outlook.com>
2020-11-17 21:04:10 -0300

    deploy 4

--- build/server.js
+++ build/server.js
@@ -1,3 +1,5 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _App = require('./App'); var _App2 = _interopRequireDefault(_App);
 
+require('dotenv').config()
+
 _App2.default.listen(process.env.PORT || 3333)
