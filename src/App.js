4e653004ca4bd7817826085000524766caf2dd13
jkevinx23 <kevingomesr@gmail.com>
2020-12-08 14:21:52 -0300

    Sentry Monit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import 'dotenv/config'
 import cron from 'node-cron'
+import Youch from 'youch'
 import express from 'express'
 import path from 'path'
 import cors from 'cors'
+
+import 'express-async-errors'
+import * as Sentry from '@sentry/node'
+import sentryConfig from './config/sentry'
 import routes from './routes'
 import './database'
+
 import ExpirarOfertas from './app/jobs/ExpirarOfertas'
 
 class App {
   constructor() {
     this.server = express()
+    Sentry.init(sentryConfig)
     this.middlewares()
     this.routes()
     this.test()
+    this.exceptionHandler()
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler())
     this.server.use(cors())
     this.server.use(express.json())
     this.server.use(
@@ -26,10 +35,19 @@ class App {
 
   routes() {
     this.server.use(routes)
+    this.server.use(Sentry.Handlers.errorHandler())
   }
 
   test() {
     cron.schedule('* * * * *', () => ExpirarOfertas.expirar())
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON()
+
+      return res.status(500).json(errors)
+    })
   }
 }
 
