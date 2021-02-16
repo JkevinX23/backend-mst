397a26e95b490e93148f900e602d7138f5e524f0
jefesonk1 <jefesonk1@outlook.com>
2021-02-16 08:09:01 -0300

    update arquivo de rotas

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import authMiddleware from './app/middlewares/auth'
 import multerConfig from './config/multerConfig'
 
 import AdministradorController from './app/controllers/AdministradorController'
-import TipoUsuariosController from './app/controllers/TipoUsuarioController'
+// import TipoUsuariosController from './app/controllers/TipoUsuarioController'
 import SessaoController from './app/controllers/SessaoController'
 import ClienteController from './app/controllers/ClienteController'
 import ImagensController from './app/controllers/ImagensController'
@@ -20,74 +20,82 @@ import storeAvailability from './app/middlewares/storeAvailability'
 import TipoFreteController from './app/controllers/TipoFreteController'
 import EsqueciASenhaController from './app/controllers/EsqueciSenhaController'
 import ContadorEstatisticaController from './app/controllers/ContadorEstatisticasController'
-// import RelatorioGeralController from './app/controllers/RelatorioGeralController'
-// import RelatorioClienteController from './app/controllers/RelatorioClienteController'
+import RelatorioPedidoGeralController from './app/controllers/RelatorioPedidoGeralController'
+import RelatorioProdutosSemanaisController from './app/controllers/RelatorioProdutosSemanaisController'
+import StatusLojaController from './app/controllers/StatusLojaController'
 
 const routes = new Router()
 const upload = multer(multerConfig)
 
-// routes.get('/relatorio-geral', RelatorioGeralController.show)
-// routes.get('/relatorio-individual', RelatorioClienteController.show)
+// !No auth routes //
+
 routes.get('/frete', TipoFreteController.index)
-
-routes.post('/administrador', AdministradorController.store)
-routes.post('/tipo-usuario', TipoUsuariosController.store)
-routes.post('/sessao', SessaoController.store)
-routes.post('/cliente', ClienteController.store)
-
-routes.post('/forget', EsqueciASenhaController.forget)
-routes.post('/reset', EsqueciASenhaController.reset)
-
 routes.get('/categoria', CategoriaController.index)
-routes.get('/tipos', TipoUsuariosController.index)
+// routes.get('/tipos', TipoUsuariosController.index)
 routes.get('/oferta', storeAvailability, OfertaController.index)
 routes.get('/tipo-pagamento', TipoPagamentosController.index)
-
 routes.get('/oferta/id/:id', OfertaController.show)
 
+routes.post('/administrador', AdministradorController.store)
+// routes.post('/tipo-usuario', TipoUsuariosController.store)
+routes.post('/sessao', SessaoController.store)
+routes.post('/cliente', ClienteController.store)
+routes.post('/forget', EsqueciASenhaController.forget)
+routes.post('/reset', EsqueciASenhaController.reset)
 routes.post(
   '/imagens',
   [adminAuth, upload.single('file')],
   ImagensController.store,
 )
+
+// !rotas com autenticação obrigatória //
 routes.use(authMiddleware)
 
-routes.get('/cliente', ClienteController.index)
-routes.get('/produto', ProdutosController.index)
-routes.get('/categoria-admin', CategoriaController.index)
-routes.get('/administrador', AdministradorController.index)
-routes.get('/validade-oferta', ValidadeOfertaController.index)
-routes.get('/oferta-admin', OfertaController.index)
-routes.get('/pedido', PedidoController.index)
+routes.get('/cliente', ClienteController.index) //! checked
+routes.get('/produto', ProdutosController.index) //! checked
+routes.get('/categoria-admin', CategoriaController.index) //! checked
+routes.get('/administrador', AdministradorController.index) //! checked
+routes.get('/validade-oferta', ValidadeOfertaController.index) //! checked
+routes.get('/oferta-admin', OfertaController.index) //! checked
+routes.get('/pedido', PedidoController.index) //! checked
+routes.get('/status-loja', StatusLojaController.index) //! checked
 
-routes.get('/estatisticas', ContadorEstatisticaController.show)
+routes.post('/produto', ProdutosController.store) //! checked
+routes.post('/pedido', PedidoController.store) //! checked
+routes.post('/tipo-pagamento', TipoPagamentosController.store) //! checked
+routes.post('/categoria', CategoriaController.store) //! checked
+routes.post('/validade-oferta', ValidadeOfertaController.store) //! checked
+routes.post('/oferta', OfertaController.store) //! checked
+routes.post('/frete', TipoFreteController.store) //! checked
+routes.post('/status-loja', StatusLojaController.store) //! checked
 
-routes.post('/produto', ProdutosController.store)
-routes.post('/pedido', PedidoController.store)
-routes.post('/tipo-pagamento', TipoPagamentosController.store)
-routes.post('/categoria', CategoriaController.store)
-routes.post('/validade-oferta', ValidadeOfertaController.store)
-routes.post('/oferta', OfertaController.store)
-routes.post('/frete', TipoFreteController.store)
+routes.put('/administrador', AdministradorController.update) //! checked
+routes.put('/categoria', CategoriaController.update) //! checked
+routes.put('/cliente', ClienteController.update) //! checked but need tests
+routes.put('/validade-oferta', ValidadeOfertaController.update) //! checked
+routes.put('/produto', ProdutosController.update) //! checked
+routes.put('/oferta', OfertaController.update) //! checked
 
-routes.put('/administrador', AdministradorController.update)
-routes.put('/categoria', CategoriaController.update)
-routes.put('/cliente', ClienteController.update)
-routes.put('/validade-oferta', ValidadeOfertaController.update)
-routes.put('/produto', ProdutosController.update)
-routes.put('/oferta', OfertaController.update)
-
-routes.get('/cliente/id/:id', ClienteController.show)
-routes.get('/administrador/id/:id', AdministradorController.show)
-routes.get('/produto/id/:id', ProdutosController.show)
+routes.get('/estatisticas', ContadorEstatisticaController.show) //! checked
+routes.get('/cliente/id/:id', ClienteController.show) //! checked
+routes.get('/administrador/id/:id', AdministradorController.show) //! checked
+routes.get('/produto/id/:id', ProdutosController.show) //! checked
+routes.get('/validade-oferta/id/:id', ValidadeOfertaController.show) //! checked
 routes.get(
   '/pedido/cliente_id/:cliente_id/pedido_id/:pedido_id',
   PedidoController.show,
-)
-routes.get('/validade-oferta/id/:id', ValidadeOfertaController.show)
+) //! checked
 
-routes.delete('/produto/:id', ProdutosController.delete)
-routes.delete('/categoria/:id', CategoriaController.delete)
-routes.delete('/oferta/:id', OfertaController.delete)
+routes.delete('/produto/:id', ProdutosController.delete) //! checked
+routes.delete('/categoria/:id', CategoriaController.delete) //! checked
+routes.delete('/oferta/:id', OfertaController.delete) //! checked
+routes.delete('/pedido/:id', PedidoController.delete) // TODO //! checked
+routes.delete('/administrador/:id', AdministradorController.delete) //! checked
+
+routes.get('/relatorio', RelatorioPedidoGeralController.gerar) //! checked
+routes.get(
+  '/relatorio-semanal-produtos',
+  RelatorioProdutosSemanaisController.gerar,
+) //! checked
 
 export default routes
