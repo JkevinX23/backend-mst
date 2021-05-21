b589d1803e6b1bc27e66bb01a4865bee24b9bd32
jkevinx23 <kevingomesr@gmail.com>
2021-05-21 16:14:44 -0300

    Divisao login admin-cliente

--- src/app/controllers/SessaoController.js
+++ src/app/controllers/SessaoController.js
@@ -31,25 +31,6 @@ class SessaoController {
 
     const { Tipo, usuario_id } = usuario
 
-    async function AdministradorLogin() {
-      const admin = await Administrador.findByPk(usuario_id)
-      if (!admin) return res.status(401).json({ error: 'User not found ' })
-      if (!(await admin.checkPassword(password)))
-        return res.status(401).json({ error: 'Password does not match ' })
-      const { id, nome } = admin
-      return res.json({
-        option: Tipo.tipo,
-        client: {
-          id,
-          nome,
-          email,
-        },
-        token: jwt.sign({ id, option: Tipo.tipo }, authConfig.secret, {
-          expiresIn: authConfig.expiresIn,
-        }),
-      })
-    }
-
     async function ClienteLogin() {
       const cliente = await Cliente.findByPk(usuario_id, {
         include: { model: enderecos, as: 'enderecos' },
@@ -81,15 +62,59 @@ class SessaoController {
     }
 
     switch (Tipo.tipo) {
-      case 'administrador':
-        return AdministradorLogin()
       case 'cliente':
         return ClienteLogin()
       default:
         return res.status(404).json({ error: 'Falha ao efetuar o login' })
     }
   }
+  async storeAdmin(req,res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    })
 
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' })
+    }
+    const { email, password } = req.body
+
+    const usuario = await Autorizacao.findOne({
+      where: { email },
+      include: [{ model: TipoUsuarios, as: 'Tipo' }],
+    })
+    if (!usuario) {
+      return res.status(404).json({ error: 'Client not found' })
+    }
+
+    const { Tipo, usuario_id } = usuario
+
+    async function AdministradorLogin() {
+      const admin = await Administrador.findByPk(usuario_id)
+      if (!admin) return res.status(401).json({ error: 'User not found ' })
+      if (!(await admin.checkPassword(password)))
+        return res.status(401).json({ error: 'Password does not match ' })
+      const { id, nome } = admin
+      return res.json({
+        option: Tipo.tipo,
+        client: {
+          id,
+          nome,
+          email,
+        },
+        token: jwt.sign({ id, option: Tipo.tipo }, authConfig.secret, {
+          expiresIn: authConfig.expiresIn,
+        }),
+      })
+    }
+
+    switch (Tipo.tipo) {
+      case 'administrador':
+        return AdministradorLogin()
+      default:
+        return res.status(404).json({ error: 'Falha ao efetuar o login' })
+    }
+  }
   async validaToken(req, res) {
     const { token } = req.body
     try {
